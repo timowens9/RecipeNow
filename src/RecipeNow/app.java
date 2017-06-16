e9549670c9fe7787da4eac0834669d058f9419af
Eric Ruppert <ericeruppert@gmail.com>
2017-06-15 21:31:09 -0400

    Eric Test

---
+++ src/RecipeNow/app.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package RecipeNow;
+
+public class app {
+    
+     public static void main(String[] args) {
+        
+        
+        System.out.println("This is a test");
+        System.out.println("This is a test from Youngmin");
+        System.out.println("Testing from Eric");
+        
+        
+    }
+    
+
+}
