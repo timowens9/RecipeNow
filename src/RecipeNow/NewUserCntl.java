cedbe22e31e085f19778f7503a6d01081de7b5f2
timothyowens <timothyowens@timothys-air.abbey.net>
2017-06-21 14:55:48 -0400

    Add Controller

---
+++ src/RecipeNow/NewUserCntl.java
@@ -0,0 +1,15 @@
+
+package RecipeNow;
+
+public class NewUserCntl {
+    
+    
+    private NewUserView userView;
+    
+    
+    public NewUserCntl(){
+        userView = new NewUserView(this);
+        userView.setVisible(true);
+    }
+    
+}
