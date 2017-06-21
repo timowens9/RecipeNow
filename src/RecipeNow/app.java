13c134aef04a4feb890b6eed07e739dc5f2bbbbb
Eric Ruppert <ericeruppert@gmail.com>
2017-06-20 21:32:49 -0400

    Database Code Update Made database remote Adjusted database names and create statements to be less loud. The databases are already created, and the test user is in the database. The login credentials are in plaintext to use.

--- src/RecipeNow/app.java
+++ src/RecipeNow/app.java
@@ -11,17 +11,12 @@ public class app {
     
      public static void main(String[] args) throws SQLException {
         
-        
-        System.out.println("This is a test");
-        
-        System.out.println("Testing from Eric");
-        System.out.println("This is a test from Youngmin #2");
-        
-        DataBaseHelper db = new DataBaseHelper();
-        db.createUserTable();
-        db.insertIntoTable(1,"TestUser","testPass");
-       // db.insertIntoTable("Tim", "dogbone");
-       // db.printTable();
+        DatabaseHelper db = new DatabaseHelper();
+        //db.createUserTable();
+        //db.insertIntoTable("TestUser","testPass");
+        // db.insertIntoTable("Tim", "dogbone");
+        db.printTable();
+        db.closeConnection();
     }
     
 
