a066fa1ad53af25ba14f0c17b15a4e5ffdac05a4
ybs5050 <bfid1111@gmail.com>
2017-06-21 15:01:06 -0500

    changed to NewUserCntl

--- src/RecipeNow/app.java
+++ src/RecipeNow/app.java
@@ -11,12 +11,26 @@ public class app {
     
      public static void main(String[] args) throws SQLException {
         
-        DatabaseHelper db = new DatabaseHelper();
+        
+        //DatabaseHelper db = new DatabaseHelper();
+        
+        //This would erase the db and create new table
         //db.createUserTable();
-        //db.insertIntoTable("TestUser","testPass");
-        // db.insertIntoTable("Tim", "dogbone");
-        db.printTable();
-        db.closeConnection();
+        
+        //Testing
+        //db.insertIntoTable("TestUser2","testPass");
+        //db.printTable();
+        //db.closeConnection();
+        
+        /*
+        username: test1
+        password: test1
+        */
+        // Start login page
+        NewUserCntl start = new NewUserCntl();
+    
+        
+        
     }
     
 
